b00db0b6c14237af7511b65ef28df46667f0591d
alekspanteli <56685380+alekspanteli@users.noreply.github.com>
2022-11-28 16:38:08 +0000

    test

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,8 @@
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
-
-const root = resolve(__dirname, 'src')
-const outDir = resolve(__dirname, 'dist')
-
-export default defineConfig({
-    root,
-    publicDir: 'assets',
+// vite.config.js
+export default {
+    root: 'src',
     build: {
-        outDir,
-        emptyOutDir: true,
-        rollupOptions: {
-            input: {
-                main: resolve(root, 'index.html'),
-                about: resolve(root, 'about.html'),
-            }
-        }
-    }
-})
+        outDir: '../dist'
+    },
+    publicDir: 'assets'
+}
